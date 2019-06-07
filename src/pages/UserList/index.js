afe0251660d23788eceeefc6d2e77e4f0402f0b1
Victor Poletti <victor.poletti@gastecnologia.com.br>
2019-06-07 14:52:44 -0300

    Listando os usuarios cadastrados no banco

--- src/pages/UserList/index.js
+++ src/pages/UserList/index.js
@@ -1,7 +1,45 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import api from '../../services/api';
 
-export default function UserList(){
+export default function UserList() {
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    async function loadUsers() {
+      const response = await api.get('/users');
+
+      setUsers(response.data);
+    }
+
+    loadUsers();
+  }, [])
+
   return (
-    <div>aa</div>
+    <table>
+      <thead>
+        <tr>
+          <th>Nome</th>
+          <th>Email</th>
+          <th>Rua</th>
+          <th>Nº</th>
+          <th></th>
+        </tr>
+      </thead>
+      <tbody>
+        {
+          users.map(user => (
+            <tr key={user._id}>
+              <td>{user.name}</td>
+              <td>{user.email}</td>
+              <td>{user.address.street}</td>
+              <td>{user.address.number}</td>
+              <td>
+                <a href="">Editar</a>
+              </td>
+            </tr>
+          ))
+        }
+      </tbody>
+    </table>
   )
 }
--- src/routes.js
+++ src/routes.js
@@ -1,16 +1,16 @@
 import React from 'react';
-import {BrowserRouter, Route, Switch} from 'react-router-dom';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
 import UserForm from './pages/UserForm';
 import UserList from './pages/UserList';
 
-export default function Routes(){
+export default function Routes() {
   return (
     <BrowserRouter>
       <Switch>
-        <Route path="/users" exact component={UserList}/>
-        <Route path="/users/create" component={UserForm}/>
-        <Route path="/users/edit/:id" component={UserForm}/>
+        <Route path="/users" exact component={UserList} />
+        <Route path="/users/create" component={UserForm} />
+        <Route path="/users/edit/:id" component={UserForm} />
       </Switch>
     </BrowserRouter>
   )
