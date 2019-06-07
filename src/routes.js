615fa6782c9dbc3c820baba447368f125b453967
Victor Poletti <victor.poletti@gastecnologia.com.br>
2019-06-07 14:30:42 -0300

    Adicionando rotas

---
+++ src/routes.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import {BrowserRouter, Route, Switch} from 'react-router-dom';
+
+import UserForm from './pages/UserForm';
+import UserList from './pages/UserList';
+
+export default function Routes(){
+  return (
+    <BrowserRouter>
+      <Switch>
+        <Route path="/users" exact component={UserList}/>
+        <Route path="/users/create" component={UserForm}/>
+        <Route path="/users/edit/:id" component={UserForm}/>
+      </Switch>
+    </BrowserRouter>
+  )
+}
